9c89c413a51bcbffa01bd4330957e673db9b5e20
Ben Wurth <ben.wurth@gmail.com>
2021-10-13 13:32:51 -0700

    Cleaning up the !stock command a bit

--- TPHA_bot/Commands/StockCommand.fs
+++ TPHA_bot/Commands/StockCommand.fs
@@ -1,16 +1,35 @@
 module TPHA_bot.Commands.StockCommand
 
-open DSharpPlus
+open DSharpPlus.Entities
 open TPHA_bot.Stocks.YFinanceService
 
-let runStockCommand (commandParts: string []) (message: Entities.DiscordMessage) : Async<Result<string, string>> =
+let runStockCommand (commandParts: string []) (message: DiscordMessage) : Async<Result<string, string>> =
+    let sendText (message: DiscordMessage) (text: string) =
+        async {
+            let! _ = message.RespondAsync(text) |> Async.AwaitTask
+            return ()
+        }
+    
+    let sendStockPrice ticker =
+        async {
+            let! stockPrice = getCurrentStockPrice ticker
+            let stockInfo = $"{ticker}: ${stockPrice}"
+            do! sendText message stockInfo
+            printfn $"Received request for stock command: {ticker} | Returned: \"{stockInfo}\""
+        }
+        
+    let sendRopeGif =
+        async {
+            do! sendText message "https://tenor.com/view/hang-suizide-desgosto-suicide-rope-gif-15405316"
+            printfn "Received request for stock command: $ROPE | Returned an edgy gif."
+        }
+    
     async {
         match List.ofArray commandParts with
         | [] -> return Error "No stock passed."
         | firstPart :: _ ->
-            let! stockPrice = getCurrentStockPrice firstPart
-            let stockInfo = $"{firstPart}: {stockPrice}"
-            let! _ = message.RespondAsync(stockInfo) |> Async.AwaitTask
-            printfn $"Received request for stock command: {firstPart} | Returned: \"{stockInfo}\""
+            match firstPart.ToLower() with
+            | "rope" -> do! sendRopeGif
+            | _ -> do! sendStockPrice firstPart
             return Ok "Message sent."
     }
